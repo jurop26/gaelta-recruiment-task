8c98c404f5602b8886fbb4e5c5010a7b8fab0214
Juraj Packa <jurop26@azet.sk>
2026-03-18 13:50:29 +0100

    received event from webhook and get order by shoptet API

--- zadanie2.js
+++ zadanie2.js
@@ -1,4 +1,26 @@
 const express = require("express");
 const app = express();
 
-app.listen();
+app.use(express.json());
+
+app.post("/shoptet-webhook", async (req, res) => {
+  const { event, eventInstance } = req.body;
+
+  if (event === "order:create") {
+    const orderCode = eventInstance;
+    const includes =
+      "?include=notes,images,shippingDetails,stockLocation,productFlags,paymentTransactions";
+    const orderUrl =
+      "https://api.myshoptet.com/api/orders/" + orderCode + includes;
+
+    const res = await fetch(orderUrl, {
+      headers: {
+        "Content-Type": "application/vnd.shoptet.v1.0",
+        "Shoptet-Private-API-Token": "apikey",
+      },
+    });
+    const order = await res.json();
+  }
+});
+
+app.listen(5000, () => console.log("Server listen on 5000"));
